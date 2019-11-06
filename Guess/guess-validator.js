4ade2d1485fba517c3c045166cb0d58048d1dbf5
pmangar <54910614+pmangar@users.noreply.github.com>
2019-11-06 18:52:27 -0500

    Update guess-validator.js

--- Guess/guess-validator.js
+++ Guess/guess-validator.js
@@ -3,32 +3,20 @@
  * By Preity Mangar. 
  */
 
-/* Global Variables */
-// Declare var answer as random integer between 1 and 100.
 var answer = Math.floor(Math.random()*100)+1;
-// Alert the answer while testing.
 console.log(answer);
-// Declare var guess, set to 0.
 var guess = 0;
 var turns = 0;
-/* Main Code */
-// Define while loop that runs while guess is not equal to answer
 while (guess != answer){
-  	// Prompt user for guess.
 	guess=prompt("Guess my number (1-100)");
     if (guess =="q") break;
 	if (validator(guess) ==true){
 	turns++;
-		// If guess lower than answer, say too low. 
-		if (guess<answer) alert("too low");
-		// Otherwise, if guess greater than answer, say too high. 
+		if (guess<answer) alert("too low"); 
 		else if (guess>answer) alert("too high");
-	// Close If block
 	}
 else alert("Invalid guess. Retry!");
-	// Close While Loop block
 }
-// If they guessed it, tell them they got it in however many turns. 
 if (guess == answer) alert("You got it in "+turns+" turns.");
 else alert("Quitter!");
 	function validator (guess){
