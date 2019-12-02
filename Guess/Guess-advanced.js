c6988df2ff38b3164998397f9135b185cdb983f6
pmangar <54910614+pmangar@users.noreply.github.com>
2019-12-02 18:03:07 -0500

    Update Guess-advanced.js

--- Guess/Guess-advanced.js
+++ Guess/Guess-advanced.js
@@ -11,43 +11,43 @@ while (again == true) {
 	game++;
 	var answer = Math.floor(Math.random()*100)+1;
 	console.log(answer);
-	while (guess != answer){
-		guess=prompt("Guess my number (1-100)");
-		if (guess =="q") {
+	while (guess != answer) {
+		guess = prompt("guess the number (1-100)");
+		if (guess == "q") {
 			alert("quitter!");
 			break;
-	}		
-	if(validator(guess) == true){
-		turns++;
-		if (guess < answer) {
-			alert("too low"); 
-		} else if (guess > answer) {
+	        }		
+	        if(validator(guess) == true) {
+			turns++;
+			if (guess < answer) {
+				alert("too low"); 
+			} else if (guess > answer) {
 			alert("too high");
+			}
+		} else if (validator(guess) == false) {
+			alert ("Invalid guess!");
 		}
-	} else if (validator(guess) == false) {
-		alert ("Invalid guess!");
-}
-if (guess == answer) {
-	gameStats(turns);
-	again = newGame();
-} else {
-	break;
-}
-			
-function validator (guess){
-	if (guess > 0 && guess < 101) {
-		return true;
-	} else { 
-		return false; 
 	}
-}
-function gameStats(turns) {
-	alert("you guessed it in"+turns+" turns");
-	totalTurns += turns;
-	averageTurns = totalTurns / games;
-	alert("you won"+game+" games with an average of" +averageTurns+" turns per game!");
-}
-function newGame() {
+	if (guess == answer) {
+		gameStats(turns);
+		again = newGame();
+	} else {
+		break;
+	}
+	function validator (guess) {
+		if (guess > 0 && guess < 101) {
+			return true;
+		} else { 
+			return false; 
+		}
+	}
+	function gameStats(turns) {
+		alert("you guessed it in"+turns+" turns");
+		totalTurns += turns;
+		averageTurns = totalTurns / games;
+		alert("you won"+game+" games with an average of" +averageTurns+" turns per game!");
+	}
+	function newGame() {
 		again = confirm("play again?");
 		if (again == false) {
 			alert("thanks for playing!");
