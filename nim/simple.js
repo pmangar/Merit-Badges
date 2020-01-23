b3f5a51e0438505353b9621fdad288b6e75dfc2a
pmangar <54910614+pmangar@users.noreply.github.com>
2020-01-23 14:17:38 -0500

    Create simple.js

---
+++ nim/simple.js
@@ -0,0 +1,40 @@
+/* Nim Super Simple */
+
+/* Globals 
+ * count = integer, winner = String
+ */ 
+var again = true 
+while(again == true){
+	 main ();
+	 again=confirm(Play Again?);
+ }
+ function main(){
+	 count = 0; turn = 0; winner = null;
+	
+	
+	while(count<21);
+	cpuTurn+ turn+ +_count
+	var turn= Math.floor(Math.random)*3)1);
+	 
+ 
+ }
+ 
+
+count += turn; 
+
+ function playerTurn(){
+	 turn>3 
+	 alert("Too high, cheater!);
+	 
+
+	count + = turn 
+	return turn;
+ }
+ 
+ function cpuTurn(){
+	 turn = random(1-3);
+	 turn+count>20&&turn>1
+	 
+	 
+	 return turn;
+ }
