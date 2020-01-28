8b9e9763b435da9e142c917b939888f34d12cece
pmangar <54910614+pmangar@users.noreply.github.com>
2020-01-28 14:24:59 -0500

    Update simple.js

--- nim/simple.js
+++ nim/simple.js
@@ -1,40 +1,35 @@
-/* Nim Super Simple */
-
-/* Globals 
- * count = integer, winner = String
- */ 
-var again = true 
+// Nim Super Duper Simple (no input validation)
+var again = true;
 while(again == true){
-	 main ();
-	 again=confirm(Play Again?);
- }
- function main(){
-	 count = 0; turn = 0; winner = null;
+	main();
+	again=confirm("Play Again?");
+}
+function main(){
+	var count = 0; var playerInput = 0; var cpuInput = 0; var winner=null;
+	while (count<21) {
+	var turn=Math.floor(Math.random()*3)+1;
+	count+=turn;
+	alert("CPU counts "+turn+". Current count is "+count+".");
+	winner="you";
+	if (count<21) {
+		turn = userTurn();
+		count+=turn;
+		alert("Current count is "+count);
+		winner="CPU";
+		}
+		}
 	
-	
-	while(count<21);
-	cpuTurn+ turn+ +_count
-	var turn= Math.floor(Math.random)*3)1);
-	 
- 
- }
- 
-
-count += turn; 
-
- function playerTurn(){
-	 turn>3 
-	 alert("Too high, cheater!);
-	 
-
-	count + = turn 
+alert("Game over. "+winner+" won!");
+}
+function cpuTurn(){
+	var turn=Math.floor(Math.random()*3)+1;
 	return turn;
- }
- 
- function cpuTurn(){
-	 turn = random(1-3);
-	 turn+count>20&&turn>1
-	 
-	 
-	 return turn;
- }
+}
+function userTurn(){
+	var turn = parseInt(prompt("Count how many? (1-3)"));
+	if (turn > 3){
+		alert("Too high, cheater!");
+		userTurn();
+	}
+	return turn;
+}
